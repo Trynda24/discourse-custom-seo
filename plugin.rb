01bf0cef75d151e56e0b6b7ba671c0985f6338a1
Trynda24 <Tryndamere2024@protonmail.com>
2025-03-09 06:55:39 -0300

    plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -24,4 +24,40 @@ after_initialize do
   add_to_serializer(:topic_view, :custom_meta_keywords, include_condition: -> { true }) do
     object.topic.custom_fields['custom_meta_keywords']
   end
+
+  # Override Discourse's automatic meta data
+  Discourse::Application.routes do
+    concern :auto_completable do
+      post :complete
+    end
+
+    resources :topics do
+      concern :auto_completable
+    end
+  end
+
+  # Add meta tags to the topic view
+  hook_listener(:header_tags) do
+    if SiteSetting.custom_seo_enabled
+      topic = @topic
+      meta_title = topic.custom_fields['custom_meta_title']
+      meta_description = topic.custom_fields['custom_meta_description']
+      meta_keywords = topic.custom_fields['custom_meta_keywords']
+
+      # Override Discourse's automatic meta title
+      if meta_title.present?
+        @title = meta_title
+      end
+
+      # Override Discourse's automatic meta description
+      if meta_description.present?
+        meta_tags += [MetaTag.tag(:name => 'description', :content => meta_description)]
+      end
+
+      # Add meta keywords tag
+      if meta_keywords.present?
+        meta_tags += [MetaTag.tag(:name => 'keywords', :content => meta_keywords)]
+      end
+    end
+  end
 end
